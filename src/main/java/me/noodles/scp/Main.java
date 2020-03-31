c7d074671c613d0e21aa2e5c60baf2ba20630b01
thinkverse <ctuvedsson@gmail.com>
2020-03-31 18:01:31 +0200

    Refactor rules command

--- src/main/java/me/noodles/scp/Main.java
+++ src/main/java/me/noodles/scp/Main.java
@@ -40,10 +40,10 @@ public class Main extends JavaPlugin implements Listener {
         this.registerCommand("discord", new Discord(this));
         this.registerCommand("fly", new FlyCommand(this));
         this.registerCommand("apply", new Apply(this));
+        this.registerCommand("rules", new Rules(this));
         this.registerCommand("twitter", new Twitter());
         this.registerCommand("website", new Website());
         this.registerCommand("store", new Store());
-        this.registerCommand("rules", new Rules());
         this.registerCommand("scphelp", new HelpMessage());
         this.registerCommand("youtubers", new YouTubersList());
         this.registerCommand("list", new List());
--- src/main/java/me/noodles/scp/messagecommands/Rules.java
+++ src/main/java/me/noodles/scp/messagecommands/Rules.java
@@ -1,45 +1,55 @@
 package me.noodles.scp.messagecommands;
 
+import me.noodles.scp.utilities.Common;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
 import me.noodles.scp.Main;
 
-public class Rules implements CommandExecutor  {
+import java.util.Collections;
+import java.util.List;
 
-	
-	String RulesMessage;
-	
-	 public Rules() {
-	        this.RulesMessage = ChatColor.translateAlternateColorCodes('&', Main.getPlugin().getConfig().getString("Messages.RulesMessage"));
-	
-	 } 
-	 
-	 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (Main.getPlugin().getConfig().getBoolean("Rules.Enabled") == true){
-		if (cmd.getName().equalsIgnoreCase("rules")) {
-			if (sender.hasPermission("scp.rules")) {
-			if (sender instanceof Player) {
-			Player p = (Player) sender;
-			p.sendMessage(String.valueOf(this.RulesMessage));
-			
-		}
-	
-			else if (Main.getPlugin().getConfig().getBoolean("Rules.Enabled") == false) {	
-			}
-			}
-}        else {
-    sender.sendMessage(ChatColor.RED + "(!) You don't have permssion to use this command!");
-}
-		}
-		return true;
+public final class Rules implements TabExecutor {
 
+	private Main plugin;
+
+	public Rules(Main plugin) {
+		this.plugin = plugin;
 	}
-}
 
+	@Override
+	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+		if (getPlugin().getConfig().getBoolean("Rules.Enabled")) {
+			if (sender instanceof Player) {
+				final Player player = (Player) sender;
+
+				if (player.hasPermission("scp.rules")) {
+					Common.tell(player, getPlugin().getConfig().getString("Messages.RulesMessage"));
+
+					return true;
+				}
+
+				Common.error(player, "You don't have permission to use this command!");
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
+		return Collections.emptyList();
+	}
+
+	public Main getPlugin() {
+		return plugin;
+	}
+
+}
 
